54c728203fec8a8019bf395898b7873ad859afa2
jaysonpulga22@gmail.com <jaysonpulga22@gmail.com>
2018-05-04 14:44:34 +0800

    remove old file of shindy

--- rest_webservices/application/controllers/Rest_server.php
+++
@@ -1,1 +0,0 @@
-<?phpdefined('BASEPATH') OR exit('No direct script access allowed');class Rest_server extends CI_Controller {    public function index()    {        $this->load->helper('url');        $this->load->view('rest_server');    }}
